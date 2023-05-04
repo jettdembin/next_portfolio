42384eec5100251d388f63b3dfc6f2a3ebd006f2
Jett <jd.dev96@gmail.com>
2023-05-03 20:41:31 -0400

    installed framer-motion and created constants folder with index.js. filled file with projects, experiences, and technologies to start

---
+++ src/app/constants/index.js
@@ -0,0 +1,159 @@
+const technologies = [
+	{
+		name: "HTML 5",
+		icon: "",
+	},
+	{
+		name: "CSS 3",
+		icon: "",
+	},
+	{
+		name: "SCSS",
+		icon: "",
+	},
+	{
+		name: "JavaScript",
+		icon: "",
+	},
+	{
+		name: "TypeScript",
+		icon: "",
+	},
+	{
+		name: "React JS",
+		icon: "",
+	},
+	{
+		name: "Next JS",
+		icon: "",
+	},
+	{
+		name: "Tailwind CSS",
+		icon: "",
+	},
+	{
+		name: "Material UI",
+		icon: "",
+	},
+	{
+		name: "Node JS",
+		icon: "",
+	},
+	{
+		name: "Prisma",
+		icon: "",
+	},
+	{
+		name: "Graphql",
+		icon: "",
+	},
+	{
+		name: "Webpack",
+		icon: "",
+	},
+	{
+		name: "git",
+		icon: "",
+	},
+	{
+		name: "figma",
+		icon: "",
+	},
+];
+
+const experiences = [
+	{
+		title: "Student",
+		company_name: "University of Central Florida",
+		icon: "",
+		iconBg: "#383E56",
+		date: "August 2015 - May 2020",
+		points: [
+			"Developing and maintaining web applications using React.js and other related technologies.",
+			"Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+			"Implementing responsive design and ensuring cross-browser compatibility.",
+			"Participating in code reviews and providing constructive feedback to other developers.",
+		],
+	},
+	{
+		title: "Customer Service Representative",
+		company_name: "Eagleview",
+		icon: "",
+		iconBg: "#E6DEDD",
+		date: "Jan 2021 - Feb 2022",
+		points: [
+			"Developing and maintaining web applications using React.js and other related technologies.",
+			"Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+			"Implementing responsive design and ensuring cross-browser compatibility.",
+			"Participating in code reviews and providing constructive feedback to other developers.",
+		],
+	},
+	{
+		title: "Frontend Developer / Designer",
+		company_name: "Gemaire LLC",
+		icon: "",
+		iconBg: "#383E56",
+		date: "May 2022 - current",
+		points: [
+			"Developing and maintaining web applications using React.js and other related technologies.",
+			"Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+			"Implementing responsive design and ensuring cross-browser compatibility.",
+			"Participating in code reviews and providing constructive feedback to other developers.",
+		],
+	},
+];
+
+const projects = [
+	{
+		name: "Your Anime",
+		description:
+			"Web application that enables users to search anime. Logged in users may add to favorites, comment, rate, add to watch list, and more!",
+		tags: [
+			{
+				name: "react",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "graphql",
+				color: "green-text-gradient",
+			},
+			{
+				name: "tailwind",
+				color: "pink-text-gradient",
+			},
+			{
+				name: "prisma",
+				color: "pink-text-gradient",
+			},
+			{
+				name: "railway",
+				color: "pink-text-gradient",
+			},
+		],
+		image: "",
+		source_code_link: "https://github.com/",
+	},
+	{
+		name: "Macro Meals",
+		description:
+			"Web-based platform that allows users to set a fitness goal and track their daily macros. Users may log in and save their daily macros to their profile page, tracking their long term progress.",
+		tags: [
+			{
+				name: "react",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "prisma",
+				color: "green-text-gradient",
+			},
+			{
+				name: "tailwind",
+				color: "pink-text-gradient",
+			},
+		],
+		image: "",
+		source_code_link: "https://github.com/",
+	},
+];
+
+export { experiences, projects, technologies };
