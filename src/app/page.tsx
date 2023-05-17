2d013c26e3da2f1b2f86753226d2ddaf879cb153
Jett <jd.dev96@gmail.com>
2023-05-16 20:04:09 -0400

    socials section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,7 +63,7 @@ export default function Home() {
 			</aside>
 			<div className="page-wrapper" ref={pageWrapperRef}>
 				<Navbar />
-				<main>
+				<main className="px-3">
 					<Hero />
 					<About />
 					<Journey />
@@ -71,6 +71,29 @@ export default function Home() {
 					<Contact />
 				</main>
 				<Footer />
+			</div>
+			<div
+				className={`socials-cntr before:absolute before:content-[""] before:border before:border-solid before:border-white before:h-32 before:left-1/2 before:bottom before:-translate-x-1/2`}
+				style={{ position: "fixed", bottom: "0", left: "1.5rem" }}
+			>
+				<div className="socials w-9 flex flex-col relative -top-20">
+					<a href="https://www.linkedin.com/in/jettdembin/" target="_blank">
+						<img
+							className="social-img filter pb-1"
+							id="bit"
+							src="/assets/socials/linkedin.svg"
+							alt="LinkedIn"
+						/>
+					</a>
+					<a href="https://www.github.com/jettdembin" target="_blank">
+						<img
+							className="social-img"
+							id="github"
+							src="/assets/github.png"
+							alt="GitHub"
+						/>
+					</a>
+				</div>
 			</div>
 			<div className="slider"></div>
 		</div>
