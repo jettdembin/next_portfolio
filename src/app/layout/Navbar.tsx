ec6a79b95e58589293d98157cfb25e38acff9ad4
Jett <jd.dev96@gmail.com>
2023-05-20 15:02:50 -0400

    added state for listening to scroll up and down to show and hide nav respecitively

--- src/app/layout/Navbar.tsx
+++ src/app/layout/Navbar.tsx
@@ -1,18 +1,51 @@
-import Image from "next/image";
-import { logo } from "../../../public/assets/index";
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { motion } from "framer-motion";
+import { fadeIn } from "../utils/motion";
 
 export default function Navbar() {
+	const [isNavShowing, setIsNavShowing] = useState(true);
+
+	useEffect(() => {
+		let prevScrollPos = window.pageYOffset;
+
+		const handleScroll = () => {
+			const currentScrollPos = window.pageYOffset;
+
+			if (prevScrollPos > currentScrollPos) {
+				setIsNavShowing(true); // Scroll up
+			} else {
+				setIsNavShowing(false); // Scroll down
+			}
+
+			prevScrollPos = currentScrollPos;
+		};
+
+		window.addEventListener("scroll", handleScroll);
+
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
+
 	return (
-		<nav className="flex items-center justify-between px-8 py-4 bg-gradient-to-r">
+		<nav className="fixed w-full flex items-center justify-between px-8 py-4 bg-gradient-to-r">
 			<div className="text-white font-bold border border-solid border-white">
 				<h2>{`</Jett>`}</h2>
 			</div>
-			<ul className="flex space-x-8">
+			<motion.ul
+				className="flex space-x-8"
+				initial="show"
+				animate={isNavShowing ? "show" : "hidden"}
+				variants={fadeIn("left", "spring", 0, 1)}
+			>
 				<li className="text-white">Home</li>
 				<li className="text-white">Projects</li>
 				<li className="text-white">About</li>
 				<li className="text-white">Contact</li>
-			</ul>
+			</motion.ul>
 		</nav>
 	);
 }
